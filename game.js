85e8554d2263effb17a25e5fa121c51fb2faa0c9
Oleksandrpr321 <158211662+Oleksandrpr321@users.noreply.github.com>
2024-02-07 20:17:31 +0200

    додали персонажа та його анімації

--- game.js
+++ game.js
@@ -31,6 +31,7 @@ function preload() {
 function create() {
     //Створюємо небо
     this.add.image(400, 300, 'sky');
+    //Додаємо платформи
     platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
@@ -38,8 +39,31 @@ function create() {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
-    //Створюємо зірочку
-    this.add.image(400, 300, 'star');
+    //Додавання персонажа та його анімацій
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+player.setBounce(0.2);
+player.setCollideWorldBounds(true);
+
+this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+});
+
+this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+});
+
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+});
 }
 
 function update() {
