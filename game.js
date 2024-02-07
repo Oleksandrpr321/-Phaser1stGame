2bac6a9a8f2a2a2319bb46d576050013c27c209e
Oleksandrpr321 <158211662+Oleksandrpr321@users.noreply.github.com>
2024-02-07 20:31:54 +0200

    Додали зірки та коментарі з нумерацією дій

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+//Конфігуруємо гру 1
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -18,6 +19,7 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
+    //Завантажили асетси 2
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
@@ -29,9 +31,9 @@ function preload() {
 }
 
 function create() {
-    //Створюємо небо
+    //Створюємо небо 3
     this.add.image(400, 300, 'sky');
-    //Додаємо платформи
+    //Додаємо платформи 4
     platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
@@ -39,7 +41,7 @@ function create() {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
-    //Додавання персонажа та його анімацій
+    //Додавання персонажа та його анімацій 5
     player = this.physics.add.sprite(100, 450, 'dude');
 
 player.setBounce(0.2);
@@ -65,12 +67,26 @@ this.anims.create({
     repeat: -1
 });
 this.physics.add.collider(player, platforms);
-//Додали курсор
+//Додали курсор 6
 cursors = this.input.keyboard.createCursorKeys();
+//Додали зірочки 8
+stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+});
+
+stars.children.iterate(function (child) {
+
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+});
+//Додали зіткнення зірок з платформами
+this.physics.add.collider(stars, platforms);
 }
 
 function update() {
-    //
+    //Управління персонажем 7
     if (cursors.left.isDown)
     {
         player.setVelocityX(-160);
